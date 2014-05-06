e4de9e1fe9157ffeb85231c7cc2f7a81db16ba6a
Matt Ray <themattray@gmail.com>
2014-05-06 11:22:26 -0400

    Removed success/error callback requirement

--- www/iBeaconGap.js
+++ www/iBeaconGap.js
@@ -4,6 +4,8 @@ exports.getBeacons = function(success, error) {
     exec(success, error, "iBeaconGap", "getBeacons", []);
 };
 
-exports.stopScanning = function(success, error) {
+exports.stopScanning = function() {
+	var success = function(){};
+	var error = function(){};
     exec(success, error, "iBeaconGap", "stopScanning", []);
 };
