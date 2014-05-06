446cd01348f4039b5b6e87718ca91a5581f43f73
Matt Ray <themattray@gmail.com>
2014-05-06 09:17:50 -0400

    Adjustments from SO

--- src/android/iBeaconGap.java
+++ src/android/iBeaconGap.java
@@ -15,101 +15,21 @@ import java.util.Collection;
 import android.util.Log;
 
 import com.radiusnetworks.ibeacon.*;
-import com.radiusnetworks.ibeacon.service.*;
 
 import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
-//import android.content.Intent;
-//import android.content.ServiceConnection;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Messenger;
-import android.os.RemoteException;
 
 public class iBeaconGap extends CordovaPlugin {
 
     protected static final String TAG = "iBeaconGap";
-//    private CallbackContext startupCallbackContext;
     private CallbackContext callbackContext;
-    
-//    private RadiusReference myRadius;
     private Context appContext;
-    
-    private IBeaconManager ibm;
-//    private Context appContext;
-    
-    private IBeaconService ibs;
-    
-    private ArrayList<IBeacon> myBeacons = new ArrayList<IBeacon>();
-    
-    private IBeaconConsumer myConsumer;
+    private BeaconUtils myUtil;
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
         appContext = this.cordova.getActivity().getApplicationContext();
-        
-        ibm = IBeaconManager.getInstanceForApplication(appContext);
-        
-        ibs = new IBeaconService();
-        
-        IBeaconManager.LOG_DEBUG = true;
-        
-        Log.d(TAG, "Availability:" + ibm.checkAvailability());
-        
-        ibm.setForegroundScanPeriod(2000);
-        ibm.setForegroundBetweenScanPeriod(2000);
-        
-        ibm.setRangeNotifier(new RangeNotifier(){
-            @Override
-            public void didRangeBeaconsInRegion(Collection<IBeacon> beacons, Region region) {
-                myBeacons.clear();
-                myBeacons.addAll(beacons);
-                Log.d(TAG, "didRangeBeaconsInRegion");
-            }
-        });
-        
-        myConsumer = new IBeaconConsumer() {
-            @Override
-            public boolean bindService(Intent arg0, ServiceConnection arg1, int arg2) {
-                // TODO Auto-generated method stub
-                ibs.bindService(arg0, arg1, arg2);
-                return true;
-            }
-
-            @Override
-            public Context getApplicationContext() {
-                // TODO Auto-generated method stub
-                return appContext;
-            }
-
-            @Override
-            public void onIBeaconServiceConnect() {
-                // TODO Auto-generated method stub
-                try {
-                    ibm.startRangingBeaconsInRegion(new Region("etg001", null, null, null));
-                } catch (RemoteException e) {   
-                    Log.d(TAG, e.getMessage());
-                }
-            }
-
-            @Override
-            public void unbindService(ServiceConnection arg0) {
-                // TODO Auto-generated method stub
-                
-            }
-        };
-        
-        ibm.bind(myConsumer);
-        
-        try {
-            ibm.startRangingBeaconsInRegion(new Region("etg001", null, null, null));
-        } catch (RemoteException e) {   
-            Log.d(TAG, e.getMessage());
-        }
-        
-        Log.d(TAG, "IBG: initializing.");
+        myUtil = new BeaconUtils(appContext);
     }
 
     @Override
@@ -119,17 +39,13 @@ public class iBeaconGap extends CordovaPlugin {
         if (action.equalsIgnoreCase("getBeacons")) {
             // String message = args.getString(0);
             // this.getBeacons(callbackContext);
-            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, listToJSONArray(myBeacons)));
+            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, listToJSONArray(myUtil.myBeacons)));
             Log.d(TAG, "IBG: getBeacons.");
             return true;
         }
         return false;
     }
-
-    protected void onCreate(Bundle savedInstanceState) {
-        // iBeaconManager.bind(this);
-    }
-
+    
     private JSONArray listToJSONArray(Collection<IBeacon> beacons) throws JSONException{
         JSONArray jArray = new JSONArray();
         for (IBeacon beacon : beacons) {
